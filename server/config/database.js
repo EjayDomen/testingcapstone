b00353ff68b8484497d71f12d7b9b8941c0fce92
Ejay <domen.ejaymatthew@gmail.com>
2024-10-24 10:59:29 +0800

    updated db config

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,15 @@
-const {Sequelize} = require('sequelize');
+const mysql = require('mysql2');
 
-const sequelize = new Sequelize('appointment', 'root', '',{
-    host:'localhost',
-    dialect: 'mysql'
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = sequelize;
+// Export the pool for use in your routes
+module.exports = pool;
