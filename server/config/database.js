e54aa85999852e3bf8987a6225c3027648181cd5
Ejay <domen.ejaymatthew@gmail.com>
2024-10-24 11:23:02 +0800

    dbupdated

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-// Create a Sequelize instance
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql', // Specifies the database dialect (mysql in this case)
+    dialect: 'mysql', // or your specific dialect like 'postgres', 'sqlite', etc.
 });
 
 module.exports = sequelize;
